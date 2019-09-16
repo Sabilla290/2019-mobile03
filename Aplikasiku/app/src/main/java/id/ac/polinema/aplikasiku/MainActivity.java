1fe24dc5ece0f7d5297fcfead0952c5095bcb8e1
Asus <sabilla290@gmail.com>
2019-09-15 20:47:46 -0700

    Membuat form activity, yang berupa: 1. LoginActivity 2. ResetPasswordActivity 3. ForgotPasswordActivity 4. SuccessLoginActivity 5. SuccessResetPasswordActivity

--- Aplikasiku/app/src/main/java/id/ac/polinema/aplikasiku/MainActivity.java
+++ Aplikasiku/app/src/main/java/id/ac/polinema/aplikasiku/MainActivity.java
@@ -5,20 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		new Handler().postDelayed(new Runnable() {
+
+			new Handler().postDelayed(new Runnable() {
 			@Override
 			public void run() {
 				Intent i = new Intent(MainActivity.this, WelcomeActivity.class);
 				startActivity(i);
 				finish();
 			}
-		}, 1000);
+			}, 1000);
 	}
 }
--- Aplikasiku/app/src/main/java/id/ac/polinema/aplikasiku/WelcomeActivity.java
+++ Aplikasiku/app/src/main/java/id/ac/polinema/aplikasiku/WelcomeActivity.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
+
 	}
 
 	public void clickLogin(View view) {
@@ -19,4 +23,6 @@ public class WelcomeActivity extends AppCompatActivity {
 		startActivity(intent);
 		finish();
 	}
+
+
 }
